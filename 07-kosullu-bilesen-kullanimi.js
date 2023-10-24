1619405e18fdec470ffb65367376a8c5223ea758
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-10-24 14:15:31 +0300

    Update 07-kosullu-bilesen-kullanimi.js

--- 07-kosullu-bilesen-kullanimi.js
+++ 07-kosullu-bilesen-kullanimi.js
@@ -1,28 +1,27 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 const ConditionalControl = () => {
-
-  const [sayi, setSayi] = useState(0)
-  
+  const [sayi, setSayi] = useState(0);
   return (
-    
     <div>
-    
-      <h3>Kullanıcının girdiği sayının 100'den büyük mü küçük mü olduğunu sorguluyoruz</h3>
-    
+      <h3>
+        Kullanıcının girdiği sayının 100'den büyük mü küçük mü olduğunu
+        sorguluyoruz
+      </h3>
       <input
         value={sayi}
-        onChange={(e) => {setSayi(e.target.value)}}
+        onChange={(e) => {
+          setSayi(e.target.value);
+        }}
       />
-          
-      {sayi < 100 && <div>Sayı 100'den küçük!</div>}
-          
-      {sayi > 100 && <div>Sayı 100'den büyük!</div>}
-        
-    </div>
-       
-  )
-       
-}
 
-export default ConditionalControl
+      {sayi < 100 ? (
+        <div>Sayı 100'den küçük!</div>
+      ) : (
+        <div>Sayı 100'den büyük!</div>
+      )}
+    </div>
+  );
+};
+
+export default ConditionalControl;
