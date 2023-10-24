8da5f866398cd11ed89809b56bb0a0efe88d48cb
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-10-24 10:02:35 +0300

    Create 02-app.js

---
+++ 02-app.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ClickMe from "./components/ClickMe";
+
+function App() {
+  return (
+    <div className="App">
+      <ClickMe />
+    </div>
+  );
+}
+
+export default App;
