b90479b597ecd6f98bf9deda08b013d02ea8e6c7
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-10-29 18:15:31 +0300

    Create 09-todos-component.js

---
+++ 09-todos-component.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Todos() {
+  return (
+    <div>
+      <ul>
+        {/*Yapılacaklar burada olacak*/}
+      </ul>
+    </div>
+  )
+}
+
+export default Todos
