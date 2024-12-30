415fa77045c13bbecb7ac36860d24cec60717c82
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-12-30 13:18:15 +0300

    Create 0004-useEffect3-App.jsx

---
+++ 0004-useEffect3-App.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+
+  const [year, setYear] = useState(2024)
+
+  const [country, setCountry] = useState("Turkiye")
+ 
+  useEffect(() => {
+    console.log("First render and (year or country state!)")
+  }, [year, country])
+
+  function handleYear() {
+    setYear(2025)
+  }
+
+  function handleCountry() {
+    setCountry("Canada")
+  }
+
+  return (
+    <>
+    <h5>useEffect Hook</h5>
+
+    <div>
+      Year: {year} and country: {country}
+    </div>
+
+    <div>
+      <button onClick={handleYear}>Change Year</button>
+      <button onClick={handleCountry}>Change Country</button>
+    </div>
+
+    </>
+  );
+}
+
+export default App;
