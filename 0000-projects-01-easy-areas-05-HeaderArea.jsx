9f7dac17cad4caa014e10a38afe592651ed4175c
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-06 10:58:58 +0300

    Create 0000-projects-01-easy-areas-05-HeaderArea.jsx

---
+++ 0000-projects-01-easy-areas-05-HeaderArea.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HeaderArea() {
+  return (
+    <div className='header-area'>
+
+        My Way: Areas & Technologies
+      
+    </div>
+  )
+}
+
+export default HeaderArea
