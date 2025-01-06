597b3b4aaa07c832bb73b8dc913e075e4e716fa0
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-01-06 10:57:13 +0300

    Create 0000-projects-01-easy-areas-03-areadata.js

---
+++ 0000-projects-01-easy-areas-03-areadata.js
@@ -0,0 +1,28 @@
+import Web3 from "./images/web3.jpg"
+import AI from "./images/ai.jpg"
+import AlgoTrade from "./images/algotrade.jpg"
+
+const data = [
+    {
+        id: 1,
+        areaname: "Web 3",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit iste repellat voluptatum nulla provident minus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit iste repellat voluptatum nulla provident minus.",
+        image: Web3
+    },
+
+    {
+        id: 2,
+        areaname: "AI",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit iste repellat voluptatum nulla provident minus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit iste repellat voluptatum nulla provident minus.",
+        image: AI
+    },
+
+    {
+        id: 3,
+        areaname: "Algo Trade",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit iste repellat voluptatum nulla provident minus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit iste repellat voluptatum nulla provident minus.",
+        image: AlgoTrade
+    },
+]
+
+export default data
